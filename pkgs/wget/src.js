568cfe7c4066c422508cf42fad7c41df51ae7358
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-03-23 10:45:53 +0000

    wget now installs to /usr/bin and now supports systemWrapper

--- pkgs/wget/src.js
+++ pkgs/wget/src.js
@@ -1,7 +1,7 @@
 // Download from web URL
-// last revision: 15/2/2025, 11:17
+// last revision: 15/2/2025, 11:17 // now 17/3/2025, 20:35
 
 async function init(args) {
     let data = await system.fetchURL(args[0])
-    system.files.writeFile(system.toDir(args[1] || "") + args[0].textAfterAll("/"), data)
+    csw.fs.write(csw.fs.toDirectory(args[1] || "") + args[0].textAfterAll("/"), data)
 }
