7c011b7d4c689d746ecf015f3409e95437a96701
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-04-16 09:49:08 +0100

    Update src.js

--- pkgs/useradd/src.js
+++ pkgs/useradd/src.js
@@ -1,7 +1,15 @@
 // create system users
 
+const users = csw.fs.read("/etc/passwd")
+
 function init(args) {
-    const system = csw.permissions.elevate()
+
+    try {
+        system
+    } catch(e) {
+        std.out = "[ERR]Sudo is required to add users."
+        return
+    }
 
     const obj = {
         password: "default",
@@ -17,13 +25,14 @@ function init(args) {
     // loop through parameters
     for (let i = 0; i < args.length; i++) {
         // check if the parameter is recognised
-        switch(args[i]) {
+
+        switch (args[i]) {
             case "--base-dir":
             case "-b":
                 i++
                 obj.baseDir = args[i]
                 break;
-            
+
             case "--comment":
             case "-c":
                 i++
@@ -35,7 +44,7 @@ function init(args) {
                 i++
                 obj.homeDir = args[i]
                 break;
-            
+
             case "--home":
             case "-d":
                 i++
@@ -67,20 +76,62 @@ function init(args) {
 
             default:
                 // if the token is the last one, it's the username
-                if (i == args.length - 1) {
-                    username = args[i]
+                if (args[i][0] == "-") {
+                    std.out = "[WRN]Unknown flag: " + args[i]
                 } else {
-                    console.warn("Unknown flag: " + args[i])
+                    username = args[i]
                 }
         }
     }
+
+    if (username == undefined) {
+        std.out = "[ERR]Username not specified."
+        throw new Error("Username not specified.")
+    }
+
     obj.homeDir = (obj.homeDir || obj.baseDir + "/" + (username || ""))
 
     try {
-        system.users.register(username, obj)
-    } catch(e) {
-        console.error("Error Creating User: " + e)
+        register(username, obj)
+    } catch (e) {
+        throw new Error("Creating User: " + e)
+        //return
     }
 }
 
-// useradd -n user -h gjersiogjrs -p -p efgehwg
+const register = function (name, object) {
+
+    const obj = JSON.parse(JSON.stringify(object))
+
+    if (users[name] !== undefined) {
+        throw new Error("user named " + name + " already exists!")
+    }
+
+    obj.userID = users.amount
+    if (obj.password == undefined) {
+        std.out += "[WRN]User password was not defined: it is set to 'default'"
+        obj.password = "default"
+    }
+    obj.password = cryptography.sha_256(obj.password)
+
+    if (obj.permissions == undefined) {
+        obj.permissions = {}
+    }
+    const p = obj.permissions
+    p.all = (p.all || false)
+    p.read = (p.read || false)
+    p.write = (p.write || false)
+    p.delete = (p.delete || false)
+
+    users[name] = obj
+    if (!csw.fs.isDirectory(obj.baseDir)) {
+        throw new Error(`User base directory (${obj.baseDir}) is not created`)
+    } else {
+        const d = obj.homeDir
+        csw.fs.createDir(d)
+        csw.fs.createDir(d + "/.profile")
+        csw.fs.createDir(d + "/.config")
+    }
+
+    return true
+}
