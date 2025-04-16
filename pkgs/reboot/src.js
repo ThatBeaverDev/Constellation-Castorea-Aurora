364660fd20451b47c1353f7eb4204a79dad185d0
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-04-16 09:41:17 +0100

    Update reboot
    - now makes sure filesystem is committed before rebooting
    - Only root can use reboot

--- pkgs/reboot/src.js
+++ pkgs/reboot/src.js
@@ -1,5 +1,15 @@
 // Restart System.
 
 async function init(args) {
+
+    try {
+        system
+    } catch(e) {
+        std.out = "[ERR]Reboot can only be ran by superuser."
+        return
+    }
+
+    await system.localFS.commit()
+    
     location.reload()
 }
