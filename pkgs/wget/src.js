2dc8f47d9c0a5f9e46c0166cefed61e1d938993c
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-04-16 09:49:13 +0100

    Update src.js

--- pkgs/wget/src.js
+++ pkgs/wget/src.js
@@ -2,6 +2,6 @@
 // last revision: 15/2/2025, 11:17 // now 17/3/2025, 20:35
 
 async function init(args) {
-    let data = await system.fetchURL(args[0])
+    let data = await csw.net.fetch(args[0])
     csw.fs.write(csw.fs.toDirectory(args[1] || "") + args[0].textAfterAll("/"), data)
 }
