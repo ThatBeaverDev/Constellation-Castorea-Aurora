56fcf742c458c0e537f5615cbd31d59f49f562a8
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-05-29 13:07:54 +0100

    Semicolons

--- kpkgs/kmisc/misc.js
+++ kpkgs/kmisc/misc.js
@@ -2,8 +2,8 @@ system.setParam = function (name, value) {
     var s = new URLSearchParams(location.search);
     s.set(String(name), String(value));
     history.replaceState("", "", "?" + s.toString());
-}
+};
 
 system.getParam = function (name) {
-    return new URLSearchParams(location.search).get(String(name))
-}
+    return new URLSearchParams(location.search).get(String(name));
+};
